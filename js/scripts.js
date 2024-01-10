725d47553c602052a2e2af4bf5d305492362704f
geremia <germysettesette@gmail.com>
2024-01-11 00:18:01 +0100

    added hovering loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,44 @@
+const TIMEOUT = 250;
+
 $(document).ready(function() {
+    // needed to istantiate images (otherwhise not loaded until shown)
     let $imageCache = $('<div class="cache" />').appendTo('body');    
+
     let imageUrls = [
         '../resources/media/shark.jpg',
         'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Raccoon_in_Central_Park_(35264).jpg/1200px-Raccoon_in_Central_Park_(35264).jpg',
     ];
-    preloadImages($imageCache, imageUrls);
+    let images = preloadImages($imageCache, imageUrls);
+    initGlitchImagesOnHover(images);
 });
 
 function preloadImages($imageCache, imageUrls) {
-    // preload images
+    // instantiate img elements inside div.cache
     let images = new Array()
     imageUrls.forEach((url) => {
         let $image = $(`<img src="${url}" />`).appendTo($imageCache);
+        images.push($image);
+    });
+
+    return images;
+}
+
+function initGlitchImagesOnHover(images) {
+    let hovering = false;
+
+    // loop
+    window.setInterval(function () {
+        if (hovering) {
+            console.log('hovering'); 
+            // TODO: change background
+        }
+    }, TIMEOUT);
+
+    // loop activation or pause
+    $('.glitch-images-on-hover').hover(
+        () => {
+            hovering = true; 
+        }, () => {
+            hovering = false;
     });
 }
